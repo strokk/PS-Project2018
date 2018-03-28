f79e0ba53c35a82b8ce9ce33a044eb7da15e7d74
strokk <35735075+strokk@users.noreply.github.com>
2018-03-28 12:25:20 +0100

    Initial code
    Added initial code that reads iris.csv

--- project.py
+++ project.py
@@ -1,2 +1,11 @@
 # Project 2018 - Programming and Scripting
 # Guilherme G. C. Paes - 2018
+
+
+#code that reads iris data from iris.csv and is nicely formatted
+
+with open("iris.csv") as f:
+    for line in f:
+        line = line.replace(',', ' ')
+        line = line.rstrip()
+        print(line[:11], line[12:16].strip())
