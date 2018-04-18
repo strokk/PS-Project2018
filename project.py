d0d0463e6be26dc66b7edcb878c607e765d58f31
strokk <35735075+strokk@users.noreply.github.com>
2018-04-18 09:34:45 +0100

    Imported libraries and initial code

--- project.py
+++ project.py
@@ -1,11 +1,24 @@
 # Project 2018 - Programming and Scripting
 # Guilherme G. C. Paes - 2018
 
+# After some research I've found out that for the purpose of this project numpy and pandas would be the best libraries to use
+# Importing the libraries:
+import numpy as np
+import pandas as pd
 
-#code that reads iris data from iris.csv and is nicely formatted
+# Importing the dataset from iris.csv file
+iris = pd.read_csv("iris.csv")
+#Printing some data just for visualization
+print(iris.head())
 
-with open("iris.csv") as f:
-    for line in f:
-        line = line.replace(',', ' ')
-        line = line.rstrip()
-        print(line[:11], line[12:16].strip())
+# Printing the rows and columns of the data using the shape property:
+print(iris.shape)
+
+#Calculating the mean of each column using numpy
+print(np.mean(iris))
+
+#Calculating the max of each column using numpy
+print(np.max(iris))
+
+#Calculating the min of each column using numpy
+print(np.min(iris))
