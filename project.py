0a9b48d383d7f276c8db675fd66399f2213976fc
strokk <35735075+strokk@users.noreply.github.com>
2018-04-21 12:19:47 +0100

    Few changes in some code, added more descriptions

--- project.py
+++ project.py
@@ -10,19 +10,19 @@ import matplotlib.pyplot as pl
 # Importing the dataset from iris.csv file
 iris = pd.read_csv("iris.csv", names = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"])
 #Printing some data just for visualization
-print(iris.head())
+print(iris.sample(10))
 
 # Printing the rows and columns of the data using the shape property:
-print(iris.shape)
+print("The iris dataset has", iris.shape, "rows and columns respectively")
 
 #Calculating the mean of each column using numpy
-print(np.mean(iris))
+print("This is the mean value for each column: \n", np.mean(iris))
 
 #Calculating the max of each column using numpy
-print(np.max(iris))
+print("This is the maximum value for each column: \n",np.max(iris))
 
 #Calculating the min of each column using numpy
-print(np.min(iris))
+print("This is the minimum value for each column: \n",np.min(iris))
 
 #For better visualization, the pandas library shows a better summary of the data, such as mean, max, min, std
 print(iris.describe())
