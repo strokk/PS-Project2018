75f6a13e77a105b405b6c444dfb17a4ae6f66b90
strokk <35735075+strokk@users.noreply.github.com>
2018-04-21 10:36:08 +0100

    Added the matplotlib library, some pandas function
    Added the describe function from pandas, also to plot histogram, and added the row names for better visualization on the iris.csv

--- project.py
+++ project.py
@@ -1,13 +1,14 @@
 # Project 2018 - Programming and Scripting
 # Guilherme G. C. Paes - 2018
 
-# After some research I've found out that for the purpose of this project numpy and pandas would be the best libraries to use
+# After some research I've found out that for the purpose of this project numpy, pandas and matplotlib would be the best libraries to use
 # Importing the libraries:
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as pl
 
 # Importing the dataset from iris.csv file
-iris = pd.read_csv("iris.csv")
+iris = pd.read_csv("iris.csv", names = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Class"])
 #Printing some data just for visualization
 print(iris.head())
 
@@ -22,3 +23,11 @@ print(np.max(iris))
 
 #Calculating the min of each column using numpy
 print(np.min(iris))
+
+#For better visualization, the pandas library shows a better summary of the data, such as mean, max, min, std
+print(iris.describe())
+
+
+# Creating a Histogram of data using matplotlib
+iris.hist()
+plt.show()
